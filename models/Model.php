7916ed69c23964d72d4650311a639e11af1fd0b3
Francis Nimpagaritse <franimpa@yahoo.fr>
2020-05-11 20:06:38 +0200

    Model database class created

---
+++ models/Model.php
@@ -0,0 +1,22 @@
+<?php
+
+abstract class Model
+{ 
+
+    protected function getPdo()
+    {       
+            try
+            {
+                $pdo = new PDO('mysql:host=' . DB_HOST .'; dbname='. DB_NAME . ';charset=utf8, ' . DB_USER . ', ' . DB_PASSWORD. ',
+            [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]');
+                
+                return $pdo;
+            }
+            catch (Exception $e)
+            {
+                die('Erreur : ' . $e->getMessage());
+            } 
+        
+    }
+
+}
