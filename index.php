21488ed1365d25f95e25704757ceac7ee42f1e9d
Francis Nimpagaritse <franimpa@yahoo.fr>
2020-05-11 17:39:24 +0200

    Our index file updated

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+session_start();
+
+//Load Config file
+require_once('config/config.php');
+
+//Load the front controller
+require_once(URLROOT.'controllers/Controller.php');
+
+//Test if Url is supplied. If not redirect to home page
+if(!isset($_GET['uc']))
+{
+    $url[0] = "accueil";
+}
+else
+{
+    //Secure url by removing all illegal URL characters from the string
+    $url = explode('/',filter_var($_GET['uc'],FILTER_SANITIZE_URL));
+    
+}
+    //Check if the first par of Url correspond to an existing controller file
+    if(file_exists('controllers/'.ucfirst(strtolower($url[0])).'Controller.php'))
+    {
+        $controller = ucfirst(strtolower($url[0]).'Controller');
+        
+        $action = isset($url[1]) ? $url[1] : "index";
+        
+        //Require the controller
+        require_once('controllers/'.$controller.'.php');   
+        
+          
+         //Instantiate the controller class
+        $controller = new $controller();                  
+        
+    //Check if second Url parameter corresponds to an action/methd of the controller
+        if(method_exists($controller,$action))
+        {
+            $id = isset($url[2]) ? $url[2] : "";
+            
+            if($id==null)
+            {                
+                $controller->$action(); 
+            }
+            else 
+            {
+                $id=(int)$id;                
+                $controller->$action($id);
+            }            
+        }         
+        else
+        { 
+            http_response_code(404);
+            echo "<h1>Erreur 404</h1>";
+            echo "L'action demandée n'existe pas"; 
+        } 
+    }
+    else
+    {
+        http_response_code(404);
+        echo "<h1>Erreur 404</h1>";
+        echo "Le contrôleur demandé n'existe pas"; 
+    } 
