b7d590ad0e77d2ddc7dbe5a7fee45178e99cb0a1
Francis Nimpagaritse <franimpa@yahoo.fr>
2020-05-26 12:56:10 +0200

    RegisterViewAndUserViewAdded

---
+++ views/admin/registerView.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Blog | Inscription</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo URL_PATH; ?>public/admin/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?php echo URL_PATH; ?>public/admin/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo URL_PATH; ?>public/admin/dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Inscripti</b>on</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Veuillez remplir le formulaire pour vous inscrire</p>
+
+      <form action="<?php echo URL_PATH ?>user/add" method="post">
+        <div class="input-group mb-3">
+          <input type="text" id="firstname" name="firstname" class="form-control <?php echo (!empty($data['firstname_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['firstname'] ?>" placeholder="Prénom">
+            <div class="input-group-append">
+              <div class="input-group-text">                
+              </div>
+            </div>
+            <span class="invalid-feedback"><?php echo $data['firstname_error']; ?></span>
+        </div>
+        <div class="input-group mb-3">
+          <input type="text" id="lastname" name="lastname" class="form-control <?php echo (!empty($data['lastname_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['lastname'] ?>" placeholder="Nom">
+            <div class="input-group-append">
+              <div class="input-group-text">                
+              </div>
+            </div>
+            <span class="invalid-feedback"><?php echo $data['lastname_error']; ?></span>
+        </div>
+        <div class="input-group mb-3">
+          <input type="email" id="email" name="email" class="form-control <?php echo (!empty($data['email_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email'] ?>" placeholder="Email">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
+            </div>
+            <span class="invalid-feedback"><?php echo $data['email_error']; ?></span>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" id="password" name="password" class="form-control <?php echo (!empty($data['password_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password'] ?>" placeholder="Mot de passe">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          <span class="invalid-feedback"><?php echo $data['password_error']; ?></span> 
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" id="confirm_password" name="confirm_password" class="form-control <?php echo (!empty($data['confirm_password_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['confirm_password'] ?>" placeholder="Confirmer mot de passe">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          <span class="invalid-feedback"><?php echo $data['password_error']; ?></span> 
+        </div>
+
+        <div class="row">
+          <div class="col-8">            
+            <a href="<?php echo URL_PATH; ?>user/login" class="btn btn-success">Déjà inscrit? Connexion
+            </a>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">M'inscrire</button>
+          </div>
+          
+          <!-- /.col -->
+          
+        </div>
+      </form>
+
+      <div class="social-auth-links text-center mb-3">
+        <p>- OU -</p>
+        
+                
+        
+      </div>
+      <!-- /.social-auth-links -->
+
+      <div class="row">        
+        <div class="col-6">
+          <a href="<?php echo URL_PATH; ?>" class="btn btn-light mt-2 text-left"><i class="fa fa-backward"></i>Retour blog</a>
+        </div>
+      </div>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="<?php echo URL_PATH; ?>public/admin/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo URL_PATH; ?>public/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo URL_PATH; ?>public/admin/dist/js/adminlte.min.js"></script>
+
+</body>
+</html>
---
+++ views/admin/userView.php
@@ -0,0 +1,170 @@
+<?php
+
+  if(!isset($_SESSION['user_id']))
+  {
+    header('Location: '. URL_PATH.'user/login');    
+      exit();
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php echo WEBSITENAME; ?> | admins</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo URL_PATH; ?>public/admin/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?php echo URL_PATH; ?>public/admin/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo URL_PATH; ?>public/admin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo URL_PATH; ?>public/admin/dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Navbar -->
+ <?php require_once('themeparts/topmenuAdmin.php'); ?>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <?php require_once('themeparts/sidebarAdmin.php'); ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h4>Gestion des utilisateurs</h4>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">User</a></li>
+              <li class="breadcrumb-item active">list</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-12">
+         
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Nos administrateurs</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Prénom</th>
+                    <th>Nom</th>
+                    <th>Email</th>
+                    <th>Profil</th>                  
+                    <th>Demande de mot de passe</th>
+                    <th>Dernière modification</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                
+                    <?php foreach($data['users'] as $user) : ?>
+                        <tr>
+                            <td><?php echo $user->getId(); ?></td>
+                            <td><?php echo $user->getFirstname(); ?></td>
+                            <td><?php echo $user->getLastname(); ?></td>
+                            <td><?php echo $user->getEmail(); ?></td>
+                            <td><?php echo $user->getProfile(); ?></td>
+                            <td><?php echo $user->new_pwd; ?></td>                            
+                            <td><?php echo $user->updated_at; ?></td>
+                            <td>
+                            <a href="<?php echo URL_PATH; ?>user/edit/<?php echo $user->getId(); ?>" class="btn btn-primary mb-2"><i class="fas fa-pencil-alt"></i></a> 
+                            <a href="<?php echo URL_PATH; ?>user/delete/<?php echo $user->getId(); ?>" class="btn btn-danger mb-2"><i class="far fa-trash-alt"></i></a>
+                            </td>
+                        </tr> 
+                    <?php endforeach ; ?>               
+                </tbody>
+                <tfoot>
+                <tr>
+                  <th>ID</th>
+                  <th>Prénom</th>
+                  <th>Nom</th>
+                  <th>Email</th>
+                  <th>Profil</th>
+                  <th>Demande de mot de passe</th>                  
+                  <th>Dernière modification</th>
+                  <th>Actions</th>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+  <?php require_once('themeparts/footerAdmin.php'); ?>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="<?php echo URL_PATH; ?>public/admin/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo URL_PATH; ?>public/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables -->
+<script src="<?php echo URL_PATH; ?>public/admin/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?php echo URL_PATH; ?>public/admin/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?php echo URL_PATH; ?>public/admin/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?php echo URL_PATH; ?>public/admin/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo URL_PATH; ?>public/admin/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo URL_PATH; ?>public/admin/dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true,
+      "autoWidth": false,
+    });
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+</body>
+</html>
