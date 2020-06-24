0577ceb0ba143927bfc5e82122126fb276335b08
Francis Nimpagaritse <franimpa@yahoo.fr>
2020-06-24 12:44:30 +0200

    template structure updated

--- views/template.php
+++ views/template.php
@@ -12,22 +12,25 @@
             
             
             
-            
             <!-- Optional JavaScript -->
             <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-            <script src="public/visitor/js/jquery-3.2.1.min.js"></script>
-            <script src="public/visitor/js/popper.js"></script>
-            <script src="public/visitor/js/bootstrap.min.js"></script>
-            <script src="public/visitor/js/stellar.js"></script>
-            <script src="public/visitor/vendors/lightbox/simpleLightbox.min.js"></script>
-            <script src="public/visitor/vendors/nice-select/js/jquery.nice-select.min.js"></script>
-            <script src="public/visitor/vendors/isotope/imagesloaded.pkgd.min.js"></script>
-            <script src="public/visitor/vendors/isotope/isotope-min.js"></script>
-            <script src="public/visitor/vendors/owl-carousel/owl.carousel.min.js"></script>
-            <script src="public/visitor/vendors/jquery-ui/jquery-ui.js"></script>
-            <script src="public/visitor/js/jquery.ajaxchimp.min.js"></script>
-            <script src="public/visitor/js/mail-script.js"></script>
-            <script src="public/visitor/js/theme.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/js/jquery-3.2.1.min.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/js/popper.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/js/bootstrap.min.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/js/stellar.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/vendors/lightbox/simpleLightbox.min.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/vendors/isotope/isotope-min.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/vendors/owl-carousel/owl.carousel.min.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/vendors/jquery-ui/jquery-ui.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/js/jquery.ajaxchimp.min.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/js/mail-script.js"></script>
+            <script src="<?=URL_PATH;?>public/visitor/js/theme.js"></script>
+
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
         </div>
     </body>
 </html>
