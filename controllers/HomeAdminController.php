33fa86affd1d8c6e371d5d7ab9196cbc26f07709
Francis Nimpagaritse <franimpa@yahoo.fr>
2020-06-24 11:32:36 +0200

    Stats added to HomeAdminController

---
+++ controllers/HomeAdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+class HomeAdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->postManager = $this->loadModel("PostManager");
+        $this->commentManager = $this->loadModel("CommentManager");
+        $this->userManager = $this->loadModel('UserManager');
+    }
+
+    public function index()
+    {
+        $posts = $this->postManager->countAllPosts();
+        $comments = $this->commentManager->countAllComments();
+        $users = $this->userManager->countAllUsers();
+        //$visitors = $this->visitorManager->countAllVisitors();
+
+        $this->loadView('admin/homeAdmin',['posts'=>$posts, 'comments'=>$comments, 'users'=>$users]);
+    }
+}
