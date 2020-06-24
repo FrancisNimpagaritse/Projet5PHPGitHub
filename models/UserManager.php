511eedd75cfcd415cb46e731c2d2f675aff22d62
Francis Nimpagaritse <franimpa@yahoo.fr>
2020-06-24 11:38:51 +0200

    UserManager PRS review

--- models/UserManager.php
+++ models/UserManager.php
@@ -10,22 +10,18 @@ class UserManager extends Model
     public function create(User &$user)
     {
         $pdo = $this->getPdo();
-        $this->pdoStmt = $pdo->prepare("INSERT INTO users(firstname, lastname, email, password) VALUES(:firstname, :lastname, :email, :password)");
-        //Bind values to params
+        $this->pdoStmt = $pdo->prepare('INSERT INTO users(firstname, lastname, email, password) VALUES(:firstname, :lastname, :email, :password)');
+        
         $this->pdoStmt->bindValue(':firstname', $user->getFirstname(), PDO::PARAM_STR);
         $this->pdoStmt->bindValue(':lastname', $user->getLastname(), PDO::PARAM_STR);        
         $this->pdoStmt->bindValue(':email', $user->getEmail(), PDO::PARAM_STR);
         $this->pdoStmt->bindValue(':password', $user->getPassword(), PDO::PARAM_STR);
-        
-        //Query Execution 
+         
         $isExecuteOk = $this->pdoStmt->execute();
 
-        if($isExecuteOk==false)
-        {
+        if ($isExecuteOk==false) {
             return false;
-        }
-        else
-        {
+        } else {
             //Get the last inserted Id
             $id = $pdo->LastInsertId();
 
@@ -34,25 +30,37 @@ class UserManager extends Model
 
             return true;
         }
-
     }
 
+    //Find all users
     public function findAll()
     {        
         $pdo = $this->getPdo();
 
-        //Query
         $this->pdoStmt = $pdo->query('SELECT * FROM users');
 
         //Initilization if objects array
-        $users = []; // If no object found then it should return empty array
+        $users = []; 
         
-        while($user = $this->pdoStmt->fetchObject('User'))
-        {
-            $users[] = $user; //Each object is added to the array of users
+        while ($user = $this->pdoStmt->fetchObject('User')) {
+           //Each object found is added to the array
+           $users[] = $user; 
+        }         
+        return $users;
+    }
+
+    //count all users
+    public function countAllUsers()
+    {
+        $pdo = $this->getPdo();
+        
+        $this->pdoStmt = $pdo->query('SELECT COUNT(*) as nbUsers FROM users');
+
+        $result = $this->pdoStmt->fetch(PDO::FETCH_OBJ);
+        if ($result == null) {
+            return [];
         }
-         //retur the array of users at the end
-         return $users;
+        return $result;
     }
 
     /* Find User by Id that return the object or null */
@@ -62,26 +70,20 @@ class UserManager extends Model
 
         $this->pdoStmt = $pdo->prepare('SELECT * FROM users WHERE id = :id');
         
-        //Param value bind
         $this->pdoStmt->bindValue(':id', $id, PDO::PARAM_INT);
 
          $isExecuteOk = $this->pdoStmt->execute();
 
-         if($isExecuteOk)
-         {
+         if ($isExecuteOk) {
             $user = $this->pdoStmt->fetchObject('User');
 
-            if($user==false)
-            {
+            if ($user==false) {
                 return null;
-            }
-            else
-            {
+            } else {
                 return $user;
             }
-         }
-         else{
-             return false; //In case of execution error
+         } else {
+             return false;
          }        
     }
 
@@ -89,67 +91,107 @@ class UserManager extends Model
     {
         $pdo = $this->getPdo();
 
-        //Requête préparée pour sécuriser les données passées en paramètres
         $this->pdoStmt = $pdo->prepare('SELECT * FROM users WHERE email = :email');
         
-        //liaison des paramètres en indiquant aussi le type de la valeur à passer
         $this->pdoStmt->bindValue(':email', $email, PDO::PARAM_STR);
 
-        //exécuter la requête
          $isExecuteOk = $this->pdoStmt->execute();
 
-         if ($isExecuteOk)
-         {
+         if ($isExecuteOk) {
             $user = $this->pdoStmt->fetchObject('User');
 
-            //On fait un test sur le retour/résultat ($user) de l'exécution de la requête
-            if ($user==false)
-            {
-                //retourne null si pas de resultat
+            if ($user==false) {
                 return null;
             } else {
-                //retourne l'object trouvé
                 return $user;
             }
          } else {
-             return false; //Erreur d'exécution
+             return false;
          }        
     }
 
-    /* -----Fonction de login------- */
+    public function findByToken($token)
+    {
+        $pdo = $this->getPdo();
+
+        $this->pdoStmt = $pdo->prepare('SELECT * FROM users WHERE token = :token');
+        
+        $this->pdoStmt->bindValue(':token', $token, PDO::PARAM_STR);
+
+         $isExecuteOk = $this->pdoStmt->execute();
+        
+         if ($isExecuteOk) {
+            $user = $this->pdoStmt->fetchObject('User');
+            
+            if ($user==null) {
+                return false;
+            } else {
+                return $user;
+            }
+         } else {
+             return false;
+         }        
+    }
+
     public function login($email, $pass)
     {
         $pdo = $this->getPdo();
 
-        //Requête préparée pour sécuriser les données passées en paramètres
         $this->pdoStmt = $pdo->prepare('SELECT * FROM users WHERE email = :email');
         
-        //liaison des paramètres en indiquant aussi le type de la valeur à passer
         $this->pdoStmt->bindValue(':email', $email, PDO::PARAM_STR);
 
-        //exécuter la requête
         $this->pdoStmt->execute();
 
         $user = $this->pdoStmt->fetchObject('User');
-
-        $hashed_password = $user->getPassword();
-        if(password_verify($pass, $hashed_password))
-        {
-            return $user;
-        } else {
+        if (!$user) {
             return false;
-        }                 
+        } else {
+            $hashed_password = $user->getPassword();
+            if (password_verify($pass, $hashed_password)) {
+                return $user;
+            } else {
+                return false;
+            }     
+        }                
     }
 
     public function update(User $user)
      {
         $pdo = $this->getPdo();
 
-        $this->pdoStmt = $pdo->prepare("UPDATE users SET firstname = :firstname, lastname = :lastname, email = :email WHERE id = :id");
+        $this->pdoStmt = $pdo->prepare('UPDATE users SET firstname = :firstname, lastname = :lastname, email = :email, updatedAt = :updatedAt WHERE id = :id');
         $this->pdoStmt->bindValue(':firstname',$user->getFirstname(),PDO::PARAM_STR);
         $this->pdoStmt->bindValue(':lastname',$user->getLastname(),PDO::PARAM_STR);
-        $this->pdoStmt->bindValue(':email',$user->getEmail(),PDO::PARAM_STR);
+        $this->pdoStmt->bindValue(':email',$user->getEmail(),PDO::PARAM_STR);        
+        $this->pdoStmt->bindValue(':updatedAt',$user->getUpdatedAt(),PDO::PARAM_STR);
         $this->pdoStmt->bindValue(':id',$user->getId(),PDO::PARAM_INT);
+
+        return $this->pdoStmt->execute();
+    }
+
+    public function updatePassword(User $user)
+     {
+        $pdo = $this->getPdo();
+
+        $this->pdoStmt = $pdo->prepare('UPDATE users SET password = :password, updatedAt = :updatedAt WHERE email = :email');
+        
+        $this->pdoStmt->bindValue(':password',$user->getPassword(),PDO::PARAM_STR);
+        $this->pdoStmt->bindValue(':email',$user->getEmail(),PDO::PARAM_STR);
+        $this->pdoStmt->bindValue(':updatedAt',$user->getUpdatedAt(),PDO::PARAM_STR);
+
+        return $this->pdoStmt->execute();
+    }
+
+    public function updateToken(User $user)
+     {
+        $pdo = $this->getPdo();
+
+        $this->pdoStmt = $pdo->prepare('UPDATE users SET token = :token, updatedAt = :updatedAt WHERE email = :email');
+        
+        $this->pdoStmt->bindValue(':token',$user->getToken(),PDO::PARAM_STR);
+        $this->pdoStmt->bindValue(':email',$user->getEmail(),PDO::PARAM_STR);        
+        $this->pdoStmt->bindValue(':updatedAt',$user->getUpdatedAt(),PDO::PARAM_STR);
 
         return $this->pdoStmt->execute();
     }
@@ -164,5 +206,4 @@ class UserManager extends Model
 
         return $this->pdoStmt->execute();
     }
-
 }
