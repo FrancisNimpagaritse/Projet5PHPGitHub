0ae5648e025e265f2a33aa2c21219b5f6fd75545
Francis Nimpagaritse <franimpa@yahoo.fr>
2020-08-10 00:26:55 +0200

    Collation for database changed to utf8_unicode_ci

--- myblog.sql
+++ myblog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  Dim 02 août 2020 à 18:01
+-- Généré le :  lun. 10 août 2020 à 00:21
 -- Version du serveur :  10.1.34-MariaDB
 -- Version de PHP :  7.2.8
 
@@ -31,27 +31,27 @@ SET time_zone = "+00:00";
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
   `postid` int(11) NOT NULL,
-  `message` text COLLATE latin1_general_ci NOT NULL,
+  `message` text COLLATE utf8_unicode_ci NOT NULL,
   `authorId` int(11) NOT NULL,
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` varchar(50) COLLATE latin1_general_ci NOT NULL DEFAULT 'attente'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  `status` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'attente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `comments`
 --
 
 INSERT INTO `comments` (`id`, `postid`, `message`, `authorId`, `createdAt`, `status`) VALUES
-(1, 1, 'Très bon article qui m\'a appris beaucoup de chose', 1, '2020-05-27 12:25:32', 'publié'),
-(2, 1, 'Post intérressant pour débuter', 1, '2020-05-27 12:28:34', 'attente'),
-(3, 1, 'PHP est super intérressant ', 2, '2020-05-27 12:28:35', 'attente'),
-(4, 3, 'J\'ai hâte découvrie Symfony après cette brève introduction. Merci à l\'auteur', 1, '2020-05-27 12:28:35', 'attente'),
+(1, 1, 'Tr?s bon article qui m\'a appris beaucoup de chose', 1, '2020-05-27 12:25:32', 'publi?'),
+(2, 1, 'Post int?rressant pour d?buter', 1, '2020-05-27 12:28:34', 'attente'),
+(3, 1, 'PHP est super int?rressant ', 2, '2020-05-27 12:28:35', 'attente'),
+(4, 3, 'J\'ai hâte d?couvrie Symfony après cette brève introduction. Merci ? l\'auteur', 1, '2020-05-27 12:28:35', 'attente'),
 (5, 4, 'Ca ne me plait pas du tout', 2, '2020-05-27 12:28:35', 'attente'),
-(6, 2, 'On aimerai voir la suite de cet article plus détaillée', 2, '2020-05-27 12:41:28', 'publié'),
+(6, 2, 'On aimerai voir la suite de cet article plus d?taill?e', 2, '2020-05-27 12:41:28', 'publi?'),
 (7, 2, 'Bravo travail bie accompli', 2, '2020-05-27 12:42:18', 'attente'),
 (8, 4, 'Bravo travail bie accompli', 2, '2020-05-27 12:43:54', 'attente'),
-(9, 4, 'Courage pour travail accompli même si on aimerait que ça soit plus technique', 2, '2020-05-27 12:43:54', 'attente'),
-(10, 4, 'Commentaire posté par Paul', 2, '2020-06-16 01:28:49', 'attente'),
+(9, 4, 'Courage pour travail accompli m?me si on aimerait que ?a soit plus technique', 2, '2020-05-27 12:43:54', 'attente'),
+(10, 4, 'Commentaire post? par Paul', 2, '2020-06-16 01:28:49', 'attente'),
 (11, 2, 'test comment', 2, '2020-07-16 12:15:12', 'attente');
 
 -- --------------------------------------------------------
@@ -62,27 +62,27 @@ INSERT INTO `comments` (`id`, `postid`, `message`, `authorId`, `createdAt`, `sta
 
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
-  `title` varchar(250) COLLATE latin1_general_ci NOT NULL,
-  `category` varchar(100) COLLATE latin1_general_ci NOT NULL,
-  `chapo` text COLLATE latin1_general_ci NOT NULL,
-  `content` text COLLATE latin1_general_ci NOT NULL,
+  `title` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `category` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `chapo` text COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci NOT NULL,
   `authorId` int(11) NOT NULL,
   `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `postImage` varchar(250) COLLATE latin1_general_ci NOT NULL,
-  `status` varchar(50) COLLATE latin1_general_ci NOT NULL DEFAULT 'attente'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  `postImage` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'attente'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `posts`
 --
 
 INSERT INTO `posts` (`id`, `title`, `category`, `chapo`, `content`, `authorId`, `updatedAt`, `postImage`, `status`) VALUES
-(1, 'PHP, langage du Web.', 'PHP', 'PHP est le langage le plus utilisé pour le développement Web.', 'PHP est le langage le plus utilisé pour le développement Web. Incontestablement le gagnant en termes de popularité. Si vous prévoyez de vous lancer dans la création de sites Web modernes, choisissez PHP sans même y penser une seconde.\r\n\r\nC&amp;#39;est le langage de programmation numéro un en matière de développement Web et il y a des raisons à cela. Je vais les détailler dans cet article et vous donner une introduction à PHP.', 1, '2020-05-27 10:59:58', 'public/visitor/img/blogposts/blog-small-01.jpg', 'publié'),
-(2, 'Boot camps have its supporters andit sdetractors.', 'Symfony', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.', 1, '2020-05-27 11:00:31', 'public/visitor/img/blogposts/blog-small-02.jpg', 'publié'),
+(1, 'PHP, langage du Web.', 'PHP', 'PHP est le langage le plus utilis? pour le d?veloppement Web.', 'PHP est le langage le plus utilis? pour le d?veloppement Web. Incontestablement le gagnant en termes de popularit?. Si vous pr?voyez de vous lancer dans la cr?ation de sites Web modernes, choisissez PHP sans m?me y penser une seconde.\r\n\r\nC&amp;#39;est le langage de programmation num?ro un en mati?re de d?veloppement Web et il y a des raisons ? cela. Je vais les d?tailler dans cet article et vous donner une introduction ? PHP.', 1, '2020-05-27 10:59:58', 'public/visitor/img/blogposts/blog-small-01.jpg', 'publi?'),
+(2, 'Boot camps have its supporters andit sdetractors.', 'Symfony', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.', 1, '2020-05-27 11:00:31', 'public/visitor/img/blogposts/blog-small-02.jpg', 'publi?'),
 (3, 'Boot camps have its supporters andit sdetractors.', 'Twig', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.', 2, '2020-05-27 11:01:03', 'public/visitor/img/blogposts/blog-small-03.jpg', 'attente'),
-(4, 'Boot camps have its supporters andit sdetractors.', 'C#/.Net', 'Lorem ipsum dolor sit amet', 'C&amp;#39;est le langage de programmation phare de Microsoft. Utilisé par un nombre important et grandissant de professionnels, il permet de réaliser toutes sortes d&amp;#39;applications.\r\nVous apprendrez comment on crée des applications informatiques et plus particulièrement celles utilisant le framework .NET que l&amp;#39;on découvrira ensemble ; puis vous vous familiariserez avec la syntaxe de base du C# pour commencer à créer des applications avec Visual Studio.', 1, '2020-05-27 11:02:06', 'public/visitor/img/blogposts/blog-small-04.jpg', 'publié'),
-(5, 'La programmation à jour', 'PHP', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.', 1, '2020-06-02 00:22:16', 'public/visitor/img/blogposts/blog-small-05.jpg', 'attente'),
-(6, 'Post number 555555Fiv', 'C#/.Net', 'mon chapo', 'Blog test pour insertion', 1, '2020-06-03 12:08:01', 'public/visitor/img/blogposts/blog-small-06.jpg', 'publié');
+(4, 'Boot camps have its supporters andit sdetractors.', 'C#/.Net', 'Lorem ipsum dolor sit amet', 'C&amp;#39;est le langage de programmation phare de Microsoft. Utilis? par un nombre important et grandissant de professionnels, il permet de r?aliser toutes sortes d&amp;#39;applications.\r\nVous apprendrez comment on cr?e des applications informatiques et plus particuli?rement celles utilisant le framework .NET que l&amp;#39;on d?couvrira ensemble ; puis vous vous familiariserez avec la syntaxe de base du C# pour commencer ? cr?er des applications avec Visual Studio.', 1, '2020-05-27 11:02:06', 'public/visitor/img/blogposts/blog-small-04.jpg', 'publi?'),
+(5, 'La programmation ? jour', 'PHP', 'Lorem ipsum dolor sit amet', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.', 1, '2020-06-02 00:22:16', 'public/visitor/img/blogposts/blog-small-05.jpg', 'attente'),
+(6, 'Post number 555555Fiv', 'C#/.Net', 'mon chapo', 'Blog test pour insertion', 1, '2020-06-03 12:08:01', 'public/visitor/img/blogposts/blog-small-06.jpg', 'publi?');
 
 -- --------------------------------------------------------
 
@@ -92,15 +92,15 @@ INSERT INTO `posts` (`id`, `title`, `category`, `chapo`, `content`, `authorId`, 
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `firstname` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `lastname` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `email` varchar(100) COLLATE latin1_general_ci NOT NULL,
-  `password` varchar(250) COLLATE latin1_general_ci NOT NULL,
-  `profile` varchar(20) COLLATE latin1_general_ci NOT NULL DEFAULT 'standard',
+  `firstname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `profile` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'standard',
   `newpwd` tinyint(1) NOT NULL DEFAULT '0',
   `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `token` varchar(250) COLLATE latin1_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  `token` varchar(250) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `users`
