07a8f735ac7486d89a3ea96e2040fba0e7cad9bd
Francis Nimpagaritse <franimpa@yahoo.fr>
2020-09-30 12:03:05 +0200

    Closing issues

--- config/config.php
+++ config/config.php
@@ -6,16 +6,9 @@ class Config
 {
     public function __construct()
     {
-        //define('URL_PATH', dirname(__DIR__, 1));
-        $dotenv = Dotenv::createImmutable(dirname(__DIR__, 1));
+       $dotenv = Dotenv::createImmutable(dirname(__DIR__, 1));
         $dotenv->load();
-        /*DB Params
-        define('DB_HOST','localhost');
-        define('DB_NAME','myblog');
-        define('DB_USER','root');
-        define('DB_PASSWORD','');
-*/
-
+        
         //App Route
         $_ENV['APPROOT_REQUIRE'] = substr($_SERVER['SCRIPT_FILENAME'], 0 , -9);
 
